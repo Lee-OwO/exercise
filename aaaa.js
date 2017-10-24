d5b69b27be97daa6c9fc5c34453acd9fa4e384d4
Lee-OwO <popobomo@outlook.com>
2017-10-24 16:18:05 +0800

    add

---
+++ aaaa.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello Node');
+}).listen(8881);
+
+console.log('Server running');
+chrome.exe --proxy-pac-url="https://b.cddm.us:5050/pac.js
+chrome.exe --proxy-pac-url="https://b.cddm.us:5050/pac.js
