ee20057c7feb5966360ed8a864a0fd2764bb75f9
Lee-OwO <popobomo@outlook.com>
2018-03-03 21:45:43 +0800

    'onload执行'

--- onload.js
+++ onload.js
@@ -1,1 +1,17 @@
-a
+var onLoad = (function() {
+	var domLoaded = false
+
+	document.addEventListener("DOMContentLoaded", function() {
+		domLoaded = true
+	})
+
+	return function onLoad(f) {
+		if (domLoaded) {
+			setTimeout(f)
+		} else {
+			document.addEventListener("DOMContentLoaded", f)
+		}
+	}
+})()
+
+// ========================================================================
